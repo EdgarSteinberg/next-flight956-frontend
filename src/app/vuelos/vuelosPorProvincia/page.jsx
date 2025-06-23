afeb30d52b583cdb70f6c0045dc12528856e0cf3
Edgar Steinberg <s.steinberg2019@gmail.com>
2025-06-23 09:50:41 -0300

    add suspense

--- src/app/vuelos/vuelosPorProvincia/page.jsx
+++ src/app/vuelos/vuelosPorProvincia/page.jsx
@@ -1,4 +1,4 @@
-import VuelosPorProvincia from "@/components/vuelos/vuelosPorProvincia"
+/* import VuelosPorProvincia from "@/components/vuelos/vuelosPorProvincia"
 
 export default function VuelosPorProvinciaPage(){
    return(
@@ -6,4 +6,17 @@ export default function VuelosPorProvinciaPage(){
         <VuelosPorProvincia/>
     </>
    )
+} */
+
+
+import { Suspense } from "react";
+import VuelosPorProvincia from "@/components/vuelos/vuelosPorProvincia";
+import Loading from "@/components/hoteles/loading";
+
+export default function VuelosPorProvinciaPage() {
+    return (
+        <Suspense fallback={<Loading />}>
+            <VuelosPorProvincia />
+        </Suspense>
+    );
 }
